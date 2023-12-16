1e088aaea11abb8ef929edd6f061e453e6823a29
luctrott <luc.trottmann@students.tbs1.de>
2023-12-16 21:24:00 +0100

    Update Testat 8.sql

--- Testat 8.sql
+++ Testat 8.sql
@@ -1,7 +1,7 @@
 SELECT strftime('%Y-%m-%d %H:00:00', datetime(command.tstamp, 'unixepoch')) AS datum, COUNT(*) AS anzahl FROM 
 	motor_command 
 		INNER JOIN command 
-		ON command.command_id = motor_command.motor_command_id 
+		ON command.command_id = motor_command.command_id 
 			INNER JOIN robot 
 			ON robot.robot_id = command.robot_id 
 				INNER JOIN motor 
@@ -9,3 +9,6 @@ SELECT strftime('%Y-%m-%d %H:00:00', datetime(command.tstamp, 'unixepoch')) AS d
 WHERE motor.name='motor_left'
 GROUP BY datum
 ORDER BY datum DESC;
+
+
+--SELECT json.data, command.tstamp FROM command INNER JOIN json ON json.json_id = command.json_id WHERE datetime(command.tstamp, 'unixepoch') BETWEEN "2023-12-16 16:00:00" AND "2023-12-16 23:00:00" AND command.robot_id=2;
